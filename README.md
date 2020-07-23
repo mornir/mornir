0ecfc2dab40777eb37e937487ba27ba1e58fdb39
mornir <jeromepott@hotmail.com>
2020-07-23 20:13:38 +0200

    Fetch articles from dev.to closes #3

--- README.md
+++ README.md
@@ -6,18 +6,32 @@
 <h2 id="mycurrentstack">🍔 My current stack</h2>
 
 <ul>
-<li>Nuxt.js</li>
+  <li>Nuxt.js</li>
 
-<li>TailwindCSS</li>
+  <li>TailwindCSS</li>
 
-<li>Sanity.io</li>
+  <li>Sanity.io</li>
 
-<li>Netlify</li>
+  <li>Netlify</li>
 
-<li>Cypress.io</li>
+  <li>Cypress.io</li>
 </ul>
 
 <h2 id="mylatestblogpostst">📝 My latest blog posts ~ 我的最新博客文章</h2>
+ 
+ this is test
+ 
+<ul>
+    <li> <a href="https:&#x2F;&#x2F;dev.to&#x2F;mornir&#x2F;faster-deploys-with-nuxt-22hi">Faster deploys with Nuxt</a></li>
+    <li> <a href="https:&#x2F;&#x2F;dev.to&#x2F;mornir&#x2F;how-to-add-mapbox-to-your-nuxt-static-site-b59">How to add Mapbox to your Nuxt static site</a></li>
+    <li> <a href="https:&#x2F;&#x2F;dev.to&#x2F;mornir&#x2F;truly-responsive-grid-with-css-grid-3c46">Truly Responsive Grid with CSS Grid</a></li>
+    <li> <a href="https:&#x2F;&#x2F;dev.to&#x2F;mornir&#x2F;nuxt-netlify-and-the-trailing-slash-3gge">Nuxt, Netlify and the trailing slash</a></li>
+    <li> <a href="https:&#x2F;&#x2F;dev.to&#x2F;mornir&#x2F;a-simpler-stack-my-experience-with-11ty-and-netlify-cms-346p">A simpler stack: my experience with 11ty and Netlify CMS</a></li>
+</ul>
+
+<b>Moe</b>
+<b>Larry</b>
+<b>Curly</b>
 
 <p><em>coming soon</em> <br />
 https://github.com/mornir/mornir/issues/3</p>
--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-// index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
-let DATA = {
-  name: 'Thomas',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Stockholm',
-  }),
-};
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
+const Mustache = require('mustache')
+const fs = require('fs')
+const got = require('got')
+const MUSTACHE_MAIN_DIR = './main.mustache'
+
+async function generateReadMe() {
+  try {
+    const articles = await got(
+      'https://dev.to/api/articles?username=mornir&per_page=5'
+    ).json()
+
+    console.log(articles)
+
+    const view = {
+      articles: articles,
+    }
+
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+      if (err) throw err
+      const output = Mustache.render(data.toString(), view)
+      fs.writeFileSync('README.md', output)
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
-generateReadMe();
+
+generateReadMe()
